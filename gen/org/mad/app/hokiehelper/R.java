560b2c4e01198dbac127d1d181bc025254594ea6
Brandon Dean <temple747@gmail.com>
2012-10-18 18:34:40 -0400

    I change these things

--- gen/org/mad/app/hokiehelper/R.java
+++ gen/org/mad/app/hokiehelper/R.java
@@ -8,7 +8,18 @@
 package org.mad.app.hokiehelper;
 
 public final class R {
+    public static final class array {
+        public static final int courses_array=0x7f060000;
+        public static final int credits_array=0x7f060002;
+        public static final int grades_array=0x7f060001;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        /**  Colors 
+         */
+        public static final int maroon=0x7f050000;
+        public static final int orange=0x7f050001;
     }
     public static final class drawable {
         public static final int action_bar=0x7f020000;
@@ -17,18 +28,64 @@ public final class R {
         public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int actionBarAddClassButton=0x7f050002;
-        public static final int actionBarIcon=0x7f050001;
-        public static final int action_bar=0x7f050000;
-        public static final int mainScrollableView=0x7f050003;
-        public static final int noClassesTextView=0x7f050004;
+        public static final int ScrollView01=0x7f07000a;
+        public static final int TableLayout0=0x7f070002;
+        public static final int TableLayout01=0x7f07000b;
+        public static final int TableLayout0_5=0x7f070006;
+        public static final int TableRow0=0x7f070003;
+        public static final int TableRow01=0x7f07000c;
+        public static final int TableRow1=0x7f070011;
+        public static final int TableRow1_1=0x7f070007;
+        public static final int TableRow1_2=0x7f070008;
+        public static final int TableRow1_3=0x7f070009;
+        public static final int TableRow2_2=0x7f07000e;
+        public static final int TableRow2_3=0x7f07000f;
+        public static final int TextView01=0x7f07000d;
+        public static final int actionBarAddClassButton=0x7f070018;
+        public static final int actionBarIcon=0x7f070017;
+        public static final int action_bar=0x7f070016;
+        public static final int btnAddRow=0x7f070004;
+        public static final int finalGPA=0x7f070013;
+        public static final int finalMainMenuBtn=0x7f070014;
+        public static final int finalTable=0x7f070010;
+        public static final int finalTxt=0x7f070012;
+        public static final int mainScrollableView=0x7f070019;
+        public static final int noClassesTextView=0x7f07001a;
+        public static final int numberOfCourses=0x7f070000;
+        public static final int startGPAOverAdd=0x7f070015;
+        public static final int startGPATermCalc=0x7f070001;
+        public static final int startGPATermFinal=0x7f070005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int main_class_view=0x7f030001;
+        public static final int gpatermadd=0x7f030000;
+        public static final int gpatermcalc=0x7f030001;
+        public static final int gpatermfinal=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int main_class_view=0x7f030004;
+        public static final int mainold=0x7f030005;
     }
     public static final class string {
+        /**  Spinners 
+         */
+        public static final int SPNnumCourses=0x7f04000b;
+        public static final int SPNnumCredits=0x7f04000d;
+        public static final int SPNnumGrade=0x7f04000c;
+        public static final int StrStartGPACalc=0x7f040005;
+        /**  directs to gpaTermAdd 
+         */
+        public static final int StrStartGPAOverall=0x7f040003;
+        public static final int StrStartGPATerm=0x7f040002;
+        public static final int addRow=0x7f040007;
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        /**  directs to gpaTermCalc 
+         */
+        public static final int calculate=0x7f040006;
+        public static final int finalGPAResult=0x7f040009;
+        public static final int finalGPATxt=0x7f040008;
+        public static final int finalMainMenuBtn=0x7f04000a;
+        public static final int proceed=0x7f040004;
+        /**  Text 
+         */
+        public static final int title=0x7f040000;
     }
 }
